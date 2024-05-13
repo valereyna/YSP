8d7361cd918ea58339cdcbb541cd368a9f732cb1
valereyna <145428872+valereyna@users.noreply.github.com>
2024-05-14 02:03:08 +0700

    updating sidebar

--- hamburger.js
+++ hamburger.js
@@ -1,13 +1,10 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const sidebar = document.querySelector('.sidebar');
 const bars = document.querySelectorAll('.bar');
+const content = document.querySelector('.contenr');
 
 hamburgerMenu.addEventListener('click', function() {
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-    }
-    else {
-        sidebar.style.width = '100vw'
-    }
+    sidebar.classList.toggle('open');
+    content.classList.toggle('open');
     bars.forEach(bar => bar.classList.toggle('change'));
 });
